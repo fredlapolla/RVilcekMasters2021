bfbad8733e1e1835116a9a1f127a74d167818d1d
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-18 09:13:54 -0400

    Add files via upload

---
+++ VilcekMasters_1_Intro_BreakOutWork.R
@@ -0,0 +1,25 @@
+# R Part 1 July 8
+
+# Break out work:
+
+## Break Out Group
+
+#On your own, create a new R Project in a new directory. A directory is just a file folder. 
+#I recommend doing this in a file folder on your computer rather than in Google Drive, as 
+#sometimes working in Drive will give you repeated annoying warnings about access. Save this 
+#somewhere that will make sense for you and name it something that will make sense later, 
+#like LearningR. 
+
+
+
+
+#Create a variable for a list of numbers 10 through 20, give it a name that makes sense to you, and find the median value. 
+
+
+
+# Write the command for pulling in a csv file. 
+
+
+#Go to Mentimeter and give a thumbs up when done.
+
+
