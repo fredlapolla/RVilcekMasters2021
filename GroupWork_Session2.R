d157b2a2d017dac5274c1570aa55010a7c81544b
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-21 11:24:28 -0400

    Add files via upload

---
+++ GroupWork_Session2.R
@@ -0,0 +1,9 @@
+## Vilcek Data Types Structures Breakout work
+
+#Breakout work Your Own 1
+
+
+#Make a factor variable out of the GENDER variable. In NYC HANES, 1 = m and 2 = f. 
+
+
+
---
+++ VilcekMasters_3_Functions_GroupWork.R
@@ -0,0 +1,41 @@
+# R Session 3 Functions Group Work
+
+## Group Work Apply 1
+
+#A Z Score is a standardized measure of how far a value is from the mean, in standardized units. The equation is 
+
+#Z = (x - mu)/sd 
+
+#X is the value in any given cell; Mu is the mean of the column or variable; Sd is the standard deviation fo that colum.
+#This can be phrased as the observation (i.e. the value in a cell) - the mean of the column or variable, divided by the standard deviation. 
+
+#Try using apply to calculate first the standard deviation, then the mean of Lead, total blood mercury, HDL and total cholesterol. 
+
+
+
+
+
+#Then using these values try to find the Z score of each cell in these columns.
+
+
+
+
+# Group Work 2
+
+#Create a dichotomous variable of diabetes diagnosis, so either has a diabetes or not (collapsing from three groups to two).
+
+
+
+## Group Work 3
+
+#Make a filtered subset of straing to only contain the aged group nyc using tidyverse's Filter command.
+
+
+
+
+
+## Group Work4 : Use of filter and operators
+
+#Use filter to retrieve a subset of only the males with diabetes in this data using filter and operators like &, |, =, or !=. 
+
+
