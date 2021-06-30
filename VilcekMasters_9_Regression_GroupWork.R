c4dcf96bb3228fbdc51a3f1d0aa7f52f4e18ed9d
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-30 16:00:52 -0400

    Add files via upload

---
+++ VilcekMasters_9_Regression_GroupWork.R
@@ -0,0 +1,8 @@
+#Regression Group Work
+
+#1. Try making a linear regression to see if there is a linear correlation with Lead as the outcome if predicted by Glucose, Age, LDL and HSQ_1 score (please note this is just an example, in real life your independent variables should have a theoretical reason for inclusion). Try a model with all the variable included and try removing variables and use the AIC to compare them. Make a plot() of your model to assess if it appears to meet the assumptions of a linear model. 
+
+
+
+
+#2. Try out running a basic logistic regression with DiabetesDichotomy as an outcome variable and A1C as the independent variables. Find the odds ratio of the coefficients. Find the confidence intervals of these odd ratios.
