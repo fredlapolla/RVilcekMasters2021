799b0ae6cb092553dc9088b75fa77550e1052aeb
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-30 15:32:37 -0400

    Add files via upload

---
+++ VilcekMasters_6_BioinformaticsVisualization_GroupWork.R
@@ -0,0 +1,11 @@
+# Group Work Bioinformatic Visualization
+
+#1. Convert the following data into a matrix, with rownames for the genes being named. Re-order the columns to group WT and KO mice strains together. Make subsets of highly expressed genes (choose your own threshold) 
+
+dolgalevNormRNA <- read.csv("https://raw.githubusercontent.com/fredlapolla/RVilcekMasters2021/main/counts.normalized.csv")
+
+
+#2. Try making a heatmap of the data you subsetted in part 1. Try both clustering by column and setting this setting to false. Explore the help information and see what other row clustering methods are available
+
+
+#3. Explore the R Color Brewer Palettes and try a different coloration for a heatmap. 
