2fde7fda8c98668ff41b0ccee1098f583ef8916b
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-30 15:50:35 -0400

    Add files via upload

---
+++ VilcekMasters_8_HypothesisTests_GroupWork.R
@@ -0,0 +1,20 @@
+#Vilcek Session 8 Group Work
+
+#1. Make a contingency table of HSQ1 vs a dichotomized Has or does not have diabetes. Run a chi square test and look up the Cranmer's V (square-root of X square divided by n * df). We can then look up online a table to see if this is a small, moderate or large effect. 
+
+
+#2.Try comparing mean LDL by if a person has diabetes or not. To do this you will need to:
+
+  #a. Collapse Diabetes into two groups
+  #b. Check that LDL Estimate appears normally distributed
+  #c, Run a T Test. 
+
+
+
+
+#3. Try comparing the means of A1C in diabetics to non-diabetics. What command will you use, and which arguments will it take?
+
+
+#4. Run a correlation of lead to to total mercury. Which test will you choose and why? What results do you get?
+
+
