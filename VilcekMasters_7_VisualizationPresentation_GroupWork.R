0d4e5fd963f1ed0f477c6c4a30fb7889c7994995
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-30 15:43:59 -0400

    Add files via upload

---
+++ VilcekMasters_7_VisualizationPresentation_GroupWork.R
@@ -0,0 +1,16 @@
+## Session 7 Group work
+
+#Make a histogram of lead levels, experiment with updating the bin width. Make a boxplot of diabetes diagnosis vs glucose level.
+
+
+
+#Try making an error bar for mean Glucose by diabetes diagnosis
+
+
+
+#Explore the palettes on R color brewer. Try changing the color scale on a scatter plot of glucose vs. A1C
+
+
+
+
+#Try adjusting the X and Y scales to be in increments of 50 for glucose, 2 for A1C in your scatter plot of A1C vs Glucose. Change the theme (background), update the X and Y labels to say "A1C level" and "Glucose level".
