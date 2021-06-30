54ead07cddf3e74e092fa32fb799c283ed16c81b
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-30 11:15:46 -0400

    Add files via upload

---
+++ VilcekMasters_4_GroupWork.R
@@ -0,0 +1,20 @@
+# Vilcek R 4 Group Work
+#Data Cleaning
+
+
+#1. Try using is.na and na.omit to remove from CADMIUM and LEAD respectively.
+
+
+
+
+#2. Try using recode_factor to change the values of DX_DBTS to "diabetes with dx", "diabetes no dx" and "no diabetes" (it's ok to abbreviate).
+
+
+
+
+
+#3. Try using Mutate to make a new variable of Total cholesterol divided by HDL. This is not a real clinical metric (as far as I know) but is intended to practice Mutate.
+
+
+
+#4. Use GREP to find only the members of K with a B. Try also using gsub to change B to BGroup.
