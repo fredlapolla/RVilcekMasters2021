5b7c0f3616503455a14bf65fb1b4a6154a0ca86a
fredlapolla <fred.lapolla@med.nyu.edu>
2021-06-30 13:57:54 -0400

    Add files via upload

---
+++ VilcekMasters_5_DataVisualization_GroupWork.R
@@ -0,0 +1,16 @@
+# Session 5 Group Work for Exploratory Data Visualization
+
+#1. Try to make a histogram of MERCURYU and LDL Estimate. Are these  normally distributed or skewed, and if so, which way?
+
+
+
+#2. Make a box plot of A1C by diabetes diagnosis. Are they likely the same for both groups?
+
+
+#3. Make a qq plot of LDL. Also make one for MercuryU. Would you expect either of these to be normal or skewed? How can you tell?
+
+
+
+#4. If we wanted to make a scatter plot of lead by cotinine, how would we do it? Based on what you now know about assessing skew, what test might we run for this?
+
+
