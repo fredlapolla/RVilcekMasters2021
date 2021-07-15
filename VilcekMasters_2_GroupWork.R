1f5af4f10d62300f89e55fdd26dc8f6a82ffcb69
fredlapolla <fred.lapolla@med.nyu.edu>
2021-07-15 09:52:43 -0400

    Add files via upload

---
+++ VilcekMasters_2_GroupWork.R
@@ -0,0 +1,23 @@
+# Break out group Let's explore some data
+
+#Find a dataset and read it in. If you do not have one, an easy option might be last week's NYC HANES data.
+
+
+
+# How to tell what kind of data you are working with
+
+#What kind of data is in your data set?
+
+
+#If looking at NYC Hanes tell what types of data are in the following:
+#nyc, the LEAD levels, the KEY, the AGEGroup, the cell in the fourth row fifth column.
+
+
+## Exploration
+
+#What kind of data are you working with?
+
+
+# Second Break out
+
+# Make a factor variable out of the GENDER variable. In NYC HANES, 1 = m and 2 = f. 
