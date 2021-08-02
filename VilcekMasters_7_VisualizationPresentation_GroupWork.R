29ae4ab908d01ca03529b979f25b58b11e74002d
fredlapolla <fred.lapolla@med.nyu.edu>
2021-08-02 08:38:31 -0400

    Add files via upload

--- VilcekMasters_7_VisualizationPresentation_GroupWork.R
+++ VilcekMasters_7_VisualizationPresentation_GroupWork.R
@@ -1,16 +1,21 @@
 ## Session 7 Group work
 
-#Make a histogram of lead levels, experiment with updating the bin width. Make a boxplot of diabetes diagnosis vs glucose level.
+#1. Try out using GGplot to make a scatter plot of A1C vs Glucose. Remember there will be two parts: where you assign the aes and then where you add a geom.
+
+
+#2. Try making the same A1C vs Glucose scatter plot but add in a line estimating linear correlation
+
+#3. Make a histogram of lead levels, experiment with updating the bin width. Make a boxplot of diabetes diagnosis vs glucose level.
 
 
 
-#Try making an error bar for mean Glucose by diabetes diagnosis
+#4. Try making an error bar for mean Glucose by diabetes diagnosis
 
 
 
-#Explore the palettes on R color brewer. Try changing the color scale on a scatter plot of glucose vs. A1C
+#5. Explore the palettes on R color brewer. Try changing the color scale on a scatter plot of glucose vs. A1C
 
 
 
 
-#Try adjusting the X and Y scales to be in increments of 50 for glucose, 2 for A1C in your scatter plot of A1C vs Glucose. Change the theme (background), update the X and Y labels to say "A1C level" and "Glucose level".
+#6. Try adjusting the X and Y scales to be in increments of 50 for glucose, 2 for A1C in your scatter plot of A1C vs Glucose. Change the theme (background), update the X and Y labels to say "A1C level" and "Glucose level".
